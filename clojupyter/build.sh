2fa0528f3783d32880f4bbcd748586283cb2147e
ivyxjc <ivyxjc1994@hotmail.com>
2021-03-30 16:31:38 +0800

    fix build.sh

--- clojupyter/build.sh
+++ clojupyter/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker build -t clojupyter-base -f Dockerfile-base .
+docker build -t clojupyter-base:0.3.2 -f Dockerfile-base .
 docker tag  clojupyter-base:0.3.2 ivymirror/clojupyter-base:0.3.2
 docker push ivymirror/clojupyter-base:0.3.2
 CLOJUPYTER_VERSION="release/0.3.2"
